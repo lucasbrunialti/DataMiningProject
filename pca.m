aa09c8a8995704073c937b4141b27cd8ee843d00
lucasbrunialti <lucas.brunialti@gmail.com>
2013-11-10 21:11:07 -0200

    pca all wrong

--- pca.m
+++ pca.m
@@ -1,7 +1,34 @@
-function [ output_args ] = pca( input_args )
-%PCA Summary of this function goes here
-%   Detailed explanation goes here
+function [ XReduced, eigenvals, eigenvecs ] = pca( X, reduceTo )
 
-
+    covX = X' * X;
+    
+    % Compute Det(covX - I*alpha): traverse the matrix diagonals multiplying the
+    % polynomials of main diagonal and suming the other diagonal
+    left = 0;
+    right = 1;
+    
+    j = size(covX, 1);
+    for i=1:size(covX, 1)
+        if (i == j)
+            left = conv(left, [-1 covX(i,i)]);
+        else
+            left = left + covX(i,j); % suming auxiliar diagonal
+        end
+        
+        right = conv(right, [-1 covX(i,i)]); % multiplying main diagonal
+        
+        j = j - 1;
+    end
+    
+    % sum left+right
+    detCovX = right;
+    detCovX(1,size(detCovX, 2)) = detCovX(1,size(detCovX, 2)) - left; 
+    
+    eigenvals = roots(detCovX);
+    
+    eigenvecs = [];
+    
+    
+    
 end
 
