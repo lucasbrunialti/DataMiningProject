0b2fe34e1387c94bb7f3983df3a1c2dad2df9ec8
lucasbrunialti <lucas.brunialti@gmail.com>
2013-11-12 01:56:49 -0200

    pca almost done

--- pca.m
+++ pca.m
@@ -1,34 +1,43 @@
-function [ XReduced, eigenvals, eigenvecs ] = pca( X, reduceTo )
-
-    covX = X' * X;
-    
-    % Compute Det(covX - I*alpha): traverse the matrix diagonals multiplying the
-    % polynomials of main diagonal and suming the other diagonal
-    left = 0;
-    right = 1;
-    
-    j = size(covX, 1);
-    for i=1:size(covX, 1)
-        if (i == j)
-            left = conv(left, [-1 covX(i,i)]);
-        else
-            left = left + covX(i,j); % suming auxiliar diagonal
-        end
-        
-        right = conv(right, [-1 covX(i,i)]); % multiplying main diagonal
-        
-        j = j - 1;
-    end
-    
-    % sum left+right
-    detCovX = right;
-    detCovX(1,size(detCovX, 2)) = detCovX(1,size(detCovX, 2)) - left; 
-    
-    eigenvals = roots(detCovX);
+function [ XReduced, eigenvals, eigenvecs ] = pca( X_aux, reduceTo )
     
     eigenvecs = [];
     
     
+    X = X_aux - repmat(mean(X_aux), size(X_aux, 1), 1);
     
-end
+    covX = X' * X;
+    
+    alpha = sym('alpha','real');
+    I = eye(size(covX,2));
+    IA = I * alpha;
+    D = covX - IA;
+    
+    p = det(D);
+    % Calculates the possible solutions for the equation
+    eigenvals = subs(solve(p));
+    
+    % For each value of alpha, calculates the eigenvecs
+    for i=1:size(X, 2)
+       
+        eigenval = eigenvals(i,1);
+        
+        wi_aux = sym('w_%d_%d', [1 size(X,2)]);
+        
+        wi_aux = subs(D, alpha, eigenval) * wi_aux';
 
+        wi = zeros(size(X,2), 1);
+        
+        wi = solve(wi_aux');
+        
+        % Normalize such that ||wi|| = 1
+        wi = wi ./ sqrt(sum( wi .^ 2 ));
+       
+        eigenvecs = [eigenvecs; wi];
+       
+    end
+    
+    Z = X * eigenvecs;
+    
+    XReduced = Z(:, reduceTo );
+
+end
