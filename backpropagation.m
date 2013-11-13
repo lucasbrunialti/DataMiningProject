914268fcb98585861b3d71101a597c1d694f20a1
Lucas Brunialti <lbrunialti@igcorp.com.br>
2013-11-13 19:06:50 -0200

    neural nets

---
+++ backpropagation.m
@@ -0,0 +1,7 @@
+function [ output_args ] = backpropagation( input_args )
+%BACKPROPAGATION Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ feedforward.m
@@ -0,0 +1,12 @@
+function [ Y ] = feedforward( X, w_in, w_hid )
+
+    Zin = [ones(size(X,1)) X] * w_in';
+
+    Z = 1 ./ ( 1 + exp(-Zin) );
+    
+    Yin = [ones(size(Z,1) Z] * w_hid';
+        
+    Y = 1 ./ ( 1 + exp(-Yin) );
+
+end
+
