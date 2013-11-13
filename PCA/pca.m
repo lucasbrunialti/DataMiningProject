dbfc0141fc0ce502f75ecfb7c43827c31e298822
Lucas Brunialti <lbrunialti@igcorp.com.br>
2013-11-13 19:46:45 -0200

    finished pca

--- PCA/pca.m
+++ PCA/pca.m
@@ -1,45 +1,12 @@
 function [ XReduced, eigenvals, eigenvecs ] = pca( X_aux, reduceTo )
     
-<<<<<<< HEAD:pca.m
+
     eigenvecs = [];
-=======
-    % Compute Det(covX - I*alpha): traverse the matrix diagonals multiplying the
-    % polynomials of main diagonal and suming the other diagonal
-    
-    syms alpha;
-    I = eye(size(covX,2));
-    IA = I * alpha;
-    D = covX - IA;
-    
-    p = det(D);
-    % Calculates the possible solutions for the equation
-    sol = solve(p);
-    
-    % For each value of alpha, calculates the reduced matrix
-    
-    left = 0;
-    right = 1;
-    
-    j = size(covX, 1);
-    for i=1:size(covX, 1)
-        if (i == j)
-            left = conv(left, [-1 covX(i,i)]);
-        else
-            left = left + covX(i,j); % suming auxiliar diagonal
-        end
-        
-        right = conv(right, [-1 covX(i,i)]); % multiplying main diagonal
-        
-        j = j - 1;
-    end
->>>>>>> 41d697f9cdfb8e4944c2d282d8d97818fc7f0345:PCA/pca.m
-    
     
     X = X_aux - repmat(mean(X_aux), size(X_aux, 1), 1);
     
     covX = X' * X;
     
-<<<<<<< HEAD:pca.m
     alpha = sym('alpha','real');
     I = eye(size(covX,2));
     IA = I * alpha;
@@ -54,18 +21,27 @@ function [ XReduced, eigenvals, eigenvecs ] = pca( X_aux, reduceTo )
        
         eigenval = eigenvals(i,1);
         
-        wi_aux = sym('w_%d_%d', [1 size(X,2)]);
+        wi_aux = sym('w_%d_%d', [size(X,2) 1]);
         
-        wi_aux = subs(D, alpha, eigenval) * wi_aux';
-
+        wi_aux = subs(D, alpha, eigenval) * wi_aux;
+        
+        wi_aux = solve(wi_aux);
+        
         wi = zeros(size(X,2), 1);
         
-        wi = solve(wi_aux');
+        
+        % Convert struct to matrix
+        fields = fieldnames(wi_aux);
+        
+        for j = 1:numel(fields)
+            wi(j,1) = subs(wi_aux.(fields{j}), 'z', 1);
+        end
+
         
         % Normalize such that ||wi|| = 1
         wi = wi ./ sqrt(sum( wi .^ 2 ));
        
-        eigenvecs = [eigenvecs; wi];
+        eigenvecs = [eigenvecs wi];
        
     end
     
@@ -73,6 +49,4 @@ function [ XReduced, eigenvals, eigenvecs ] = pca( X_aux, reduceTo )
     
     XReduced = Z(:, reduceTo );
 
-=======
->>>>>>> 41d697f9cdfb8e4944c2d282d8d97818fc7f0345:PCA/pca.m
 end
